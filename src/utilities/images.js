67969b8ff9d6d253e6206ec10fcb7408f995dbd8
ditinex <asifakramsk@gmail.com>
2021-03-25 11:22:10 +0530

    repo merged

---
+++ src/utilities/images.js
@@ -0,0 +1,47 @@
+// Authentication
+import OpenEyePassword from '../assets/images/signup/eyeOpen.png';
+import CloseEyePassword from '../assets/images/signup/eyeCrossed.png';
+import TowheelText from '../assets/images/signup/towheel.png';
+import FacebookIcon from '../assets/images/social-icons/facebook.png';
+import AppleIcon from '../assets/images/social-icons/apple.png';
+import GoogleIcon from '../assets/images/social-icons/google.png';
+import SendCode from '../assets/images/verification/sendCode.png';
+import HandSet from '../assets/images/verification/handset.png';
+import USAFlag from '../assets/images/verification/usaFlag.png';
+import VerifyCode from '../assets/images/verification/verifyCode.png';
+
+
+//Home
+import TowTruck from '../assets/images/homescreen/towTruck.png';
+import MarkerFreeTowTruck from '../assets/images/homescreen/freeTowCarMarker.png';
+import MarkerBusyTowTruck from '../assets/images/homescreen/busyTowCarMarker.png';
+import EndOFDirectionMarker from '../assets/images/homescreen/endOfDirectionMarker.png'
+import Avatar from '../assets/images/homescreen/avatar.gif';
+import AvatarTomer from '../assets/images/homescreen/avatar_Tomer.png';
+
+import ArrowLeftBlack from '../assets/images/homescreen/arrowLeftBlack.png';
+
+
+export default {
+    login: {
+        openEye: OpenEyePassword,
+        closedEye: CloseEyePassword,
+        apple: AppleIcon,
+        facebook: FacebookIcon,
+        google: GoogleIcon,
+        sendCode: SendCode,
+        handSet: HandSet,
+        usaFlag: USAFlag,
+        verifyCode: VerifyCode,
+    },
+    logo: TowheelText,
+    homescreen:{
+        towTruck: TowTruck,
+        markerFreeTowTruck: MarkerFreeTowTruck,
+        markerBusyTowTruck: MarkerBusyTowTruck,
+        endOFDirectionMarker: EndOFDirectionMarker,
+        avatar: Avatar,
+        avatarTomer: AvatarTomer,
+        arrowLeftBlack: ArrowLeftBlack
+    }
+}
